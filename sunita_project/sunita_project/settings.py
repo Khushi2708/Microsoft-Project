1007554773fb839c249ff7072cdacd5930fe5c52
Khushi Gupta <93420623+Khushi2708@users.noreply.github.com>
2022-06-11 21:17:15 +0530

    Update settings.py

--- sunita_project/sunita_project/settings.py
+++ sunita_project/sunita_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-g((s%4(*^u-+j^1x^fl2b)&=h4&so%k%&vc@3a-vb9g5ghd*^4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['khushisapp.azurewebsites.net]
 
 
 # Application definition
